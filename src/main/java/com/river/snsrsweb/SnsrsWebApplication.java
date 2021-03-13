833f6495e2a472995f480d6af1f7385966714d1e
seokh1213 <seokh1213@gmail.com>
2021-03-13 17:13:50 +0900

    init

---
+++ src/main/java/com/river/snsrsweb/SnsrsWebApplication.java
@@ -0,0 +1,13 @@
+package com.river.snsrsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsrsWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnsrsWebApplication.class, args);
+    }
+
+}
