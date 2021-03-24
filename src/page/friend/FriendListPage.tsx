66e36bd78f1b37ba318c2da17021a9e5591e0c94
seokh1213 <seokh1213@gmail.com>
2021-03-24 12:19:41 +0900

    bug

--- src/page/friend/FriendListPage.tsx
+++ src/page/friend/FriendListPage.tsx
@@ -13,10 +13,12 @@ interface IData {
     error?: any;
     loading: boolean;
     page: number;
+    total: number;
   };
 }
 
 const FriendListPage = () => {
+  const token = localStorage.getItem("token");
   const [currentType, setType] = React.useState(0);
   const [shouldFetch, setShouldFetch] = React.useState(true);
   const [data, setData] = React.useState<IData>({
@@ -24,24 +26,83 @@ const FriendListPage = () => {
       list: [],
       error: null,
       loading: false,
-      page: 1,
+      page: 0,
+      total: 0,
     },
     1: {
       list: [],
       error: null,
       loading: false,
-      page: 1,
+      page: 0,
+      total: 0,
     },
     2: {
       list: [],
       error: null,
       loading: false,
-      page: 1,
+      page: 0,
+      total: 0,
     },
   });
 
-  const follow = (id: number) => {};
-  const unfollow = (id: number) => {};
+  const follow = (id: number) => {
+    fetch(process.env.REACT_APP_API_URL + "/follow?opponent=" + id, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "x-access-token": token!,
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+      });
+  };
+  const unfollow = (id: number) => {
+    fetch(process.env.REACT_APP_API_URL + "/unfollow?opponent=" + id, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "x-access-token": token!,
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+      });
+  };
+  const fetchList = () => {
+    const urlMap = ["/recommend", "/follows", "/followers"];
+    fetch(
+      process.env.REACT_APP_API_URL +
+        urlMap[currentType] +
+        "?page=" +
+        (data[currentType].page + 1),
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "x-access-token": token!,
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        if (json.ok) {
+          setData({
+            ...data,
+            [currentType]: {
+              loading: false,
+              list: [...data[currentType].list, ...json.list],
+              error: null,
+              page: json.page,
+              total: json.total,
+            },
+          });
+        }
+      });
+  };
 
   React.useEffect(() => {
     if (shouldFetch) {
@@ -52,41 +113,9 @@ const FriendListPage = () => {
           loading: true,
         },
       });
-      // debug
-      setTimeout(() => {
-        setData({
-          ...data,
-          [currentType]: {
-            ...data[currentType],
-            loading: false,
-            list: [
-              ...data[currentType].list,
-              ...[
-                { id: 1, phoneNumber: "1234", isFollow: false },
-                { id: 2, phoneNumber: "5678", isFollow: false },
-                { id: 3, phoneNumber: "asfasdf", isFollow: false },
-                { id: 4, phoneNumber: "4vi8c", isFollow: false },
-                { id: 5, phoneNumber: "39394j", isFollow: false },
-                { id: 6, phoneNumber: "58976", isFollow: false },
-                { id: 7, phoneNumber: "181218", isFollow: false },
-                { id: 8, phoneNumber: "38172", isFollow: false },
-                { id: 9, phoneNumber: "10981", isFollow: false },
-                { id: 10, phoneNumber: "9123", isFollow: false },
-                { id: 11, phoneNumber: "58923", isFollow: false },
-                { id: 12, phoneNumber: "48593", isFollow: false },
-                { id: 13, phoneNumber: "35u213", isFollow: false },
-                { id: 14, phoneNumber: "581739275", isFollow: false },
-                { id: 15, phoneNumber: "981723", isFollow: false },
-                { id: 16, phoneNumber: "9812371", isFollow: false },
-                { id: 17, phoneNumber: "81638712", isFollow: false },
-                { id: 18, phoneNumber: "81234", isFollow: false },
-                { id: 19, phoneNumber: "1534", isFollow: false },
-                { id: 20, phoneNumber: "4444", isFollow: false },
-              ],
-            ],
-          },
-        });
-      }, 1000);
+
+      fetchList();
+
       setShouldFetch(false);
     }
   }, [shouldFetch]);
